37b65ae914788d995b15c32e16614a825c14d237
Lucy Fletcher <LucyKFletcher@gmail.com>
2018-06-17 20:17:29 +0100

    Updating max file upload sizes

--- photography/photography/settings/production.py
+++ photography/photography/settings/production.py
@@ -9,6 +9,8 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+WAGTAILIMAGES_MAX_UPLOAD_SIZE = 512 * 1024 * 1024  # i.e. 512MB
+
 try:
     from .local import *
 except ImportError:
