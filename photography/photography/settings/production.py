f8c27d13e834373e782bf1fd0a9fd17a284f0a04
Lucy Fletcher <LucyKFletcher@gmail.com>
2018-12-16 16:32:23 +0000

    Made nginx serve uploaded media files

--- photography/photography/settings/production.py
+++ photography/photography/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["lucyfletcher.photography","localhost"]
+ALLOWED_HOSTS = ["lucyfletcher.photography", "localhost"]
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
